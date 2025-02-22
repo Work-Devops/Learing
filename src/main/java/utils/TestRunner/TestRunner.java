5a7e230d9a1c274630ea6f4542647144901edf4e
Mukesh Jha <mukeshjha@Mukeshs-MacBook-Air.local>
2025-02-22 22:33:21 +0530

    this is mny final commit

--- src/main/java/utils/TestRunner/TestRunner.java
+++ src/main/java/utils/TestRunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-  features = "src/test/resources/features",
+  features = "src/test/resources/features/Changes",
   glue = "stepdefinitions"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
