db4d823e6bc9b050ac192a00c1265cf1b685e5d3
Mukesh Jha <mukeshjha@Mukeshs-MacBook-Air.local>
2025-02-22 22:45:19 +0530

    this is the main

--- src/main/java/utils/TestRunner/TestRunner.java
+++ src/main/java/utils/TestRunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-  features = "src/test/resources/features/Changes",
+  features = "src/test/resources/features/Changes/addvalues",
   glue = "stepdefinitions"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
